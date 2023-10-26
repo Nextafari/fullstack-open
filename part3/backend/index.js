9659812fad9c89cb94d781c546077c80f0008f60
Nextafari <chinedue16@gmail.com>
2023-10-26 18:17:16 +0100

    Update: modify vite config, remove dist from .gitignore

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -5,6 +5,7 @@ const morgan = require('morgan')
 morgan.token('req-body', (req, res) => {return JSON.stringify(req.body)})
 
 app.use(express.json())
+app.use(express.static('dist'))
 app.use(
   morgan(
     ':method :url :status :res[content-length] - :response-time ms :req-body'
