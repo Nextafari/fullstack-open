9824658f5193b11bf1aadc720a2198aec115bb07
Nextafari <chinedue16@gmail.com>
2023-11-09 17:55:47 +0100

    Completed exercise 4.1-4.2

---
+++ part4/bloglist/app.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+const morgan = require('morgan')
+const config = require('./utils/config')
+const blogsRouter = require('./controllers/blogs')
+
+
+
+const mongoUrl = config.DB_URL
+mongoose.connect(mongoUrl).then(
+    (result) => {
+      console.log("connected to  MongoDB")
+    }
+).catch(
+    (error) => {
+      console.log('error connecting to MongoDB:', error.messaage)
+    }
+)
+
+morgan.token('req-body', (req) => {return JSON.stringify(req.body)})
+
+app.use(cors())
+app.use(express.json())
+app.use(express.static('dist'))
+app.use(
+  morgan(
+    ':method :url :status :res[content-length] - :response-time ms :req-body'
+  )
+)
+app.use('/api/blogs', blogsRouter)
+
+module.exports = app
---
+++ part4/bloglist/controllers/blogs.js
@@ -0,0 +1,25 @@
+const blogsRouter = require('express').Router()
+const Blog = require('../model/blog')
+
+
+blogsRouter.get('/', (request, response, next) => {
+  Blog
+    .find({})
+    .then(blogs => {
+      response.json(blogs)
+    })
+    .catch(error => next(error.message))
+})
+
+blogsRouter.post('/', (request, response, next) => {
+  const blog = new Blog(request.body)
+
+  blog
+    .save()
+    .then(result => {
+      response.status(201).json(result)
+    })
+    .catch(error => next(error.message))
+})
+
+module.exports = blogsRouter
