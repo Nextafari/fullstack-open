973fe90ab10a960577ce648db309a8f2b853bc3d
Nextafari <chinedue16@gmail.com>
2023-11-14 12:09:06 +0100

    Completed Exercises 4.3.-4.7.

--- part4/bloglist/tests/blogs.test.js
+++ part4/bloglist/tests/blogs.test.js
@@ -12,7 +12,7 @@ describe('total likes', () => {
            "_id":{
               "$oid":"654d103993f973ae85b2fdd3"
            },
-           "title":"Do more expect little.",
+           "title":"Foo Bar",
            "author":"Mark Dwanye",
            "url":"http://localhost:3001/api/persons/654439ba19dfe87abd53097f",
            "likes": 3,
@@ -20,47 +20,82 @@ describe('total likes', () => {
         }
     ]
     const blogs = [
-        {
-           "_id":{
-              "$oid":"654d103993f973ae85b2fdd3"
-           },
-           "title":"Do more expect little.",
-           "author":"Mark Dwanye",
-           "url":"http://localhost:3001/api/persons/654439ba19dfe87abd53097f",
-           "likes": 3,
-           "__v": 0
-        },
-        {
-           "_id":{
-              "$oid":"654d0e8f270bb19ab3a855c1"
-           },
-           "title":"Do more expect little.",
-           "author":"222-439-7676",
-           "url":"http://localhost:3001/api/persons/654439ba19dfe87abd53097f",
-           "likes":3,
-           "__v":0
-        },
-        {
-           "_id":{
-              "$oid":"654d0e8f270bb19ab3a855c1"
-           },
-           "title":"Do more expect little.",
-           "author":"222-439-7676",
-           "url":"http://localhost:3001/api/persons/654439ba19dfe87abd53097f",
-           "likes": 6,
-           "__v":0
-        }
-    ]
+      {
+         "_id":{
+            "$oid":"654d103993f973ae85b2fdd3"
+         },
+         "title":"Do more expect little.",
+         "author":"Mark Dwanye",
+         "url":"http://localhost:3001/api/persons/654439ba19dfe87abd53097f",
+         "likes": 3,
+         "__v": 0
+      },
+      {
+         "_id":{
+            "$oid":"654d0e8f270bb19ab3a855c1"
+         },
+         "title":"How far e dey go",
+         "author":"Forester Brail",
+         "url":"http://localhost:3001/api/persons/654439ba19dfe87abd53097f",
+         "likes":3,
+         "__v":0
+      },
+      {
+         "_id":{
+            "$oid":"654d0e8f270bb19ab3a855c1"
+         },
+         "title":"Able God",
+         "author":"Chinko Ekun",
+         "url":"http://localhost:3001/api/persons/654439ba19dfe87abd53097f",
+         "likes": 6,
+         "__v":0
+      },
+      {
+         "_id":{
+            "$oid":"654d0e8f270bb19ab3a855c1"
+         },
+         "title":"Life in the trenches",
+         "author":"Chinko Ekun",
+         "url":"http://localhost:3001/api/persons/654439ba19dfe87abd53097f",
+         "likes": 6,
+         "__v":0
+      },
+      {
+         "_id":{
+            "$oid":"654d0e8f270bb19ab3a855c1"
+         },
+         "title":"Life in the Deserts",
+         "author":"Habibi",
+         "url":"http://localhost:3001/api/persons/654439ba19dfe87abd53097f",
+         "likes": 6,
+         "__v":0
+      }
+   ]
 
-    test('when list hs only one blog, equals the likes of that', () => {
-        const result = listHelper.totalLikes(listWithOneBlog)
-        expect(result).toBe(3)
-    })
-    test('favourite blog', () => {
-        const result = listHelper.favouriteBlog(blogs)
-        expect(result).toEqual({
-            "author":"222-439-7676",
-            "likes": 6
-        })
-    })
+   test('when list has only one blog, equals the likes of that', () => {
+      const result = listHelper.totalLikes(listWithOneBlog)
+      expect(result).toBe(3)
+   })
+   test('favourite blog', () => {
+      const result = listHelper.favouriteBlog(blogs)
+      expect(result).toEqual({
+         "title":"Able God",
+         "author":"Chinko Ekun",
+         "likes": 6
+      })
+   })
+   test('Test Author with most blogs', () => {
+      const result = listHelper.mostBlogs(blogs)
+      expect(result).toEqual({
+         author: "Chinko Ekun",
+         blogs: 2
+      })
+   })
+   test('Test Author with most likes', () => {
+      const result = listHelper.mostLikes(blogs)
+      expect(result).toEqual({
+         author: "Chinko Ekun",
+         likes: 12
+      })
+   })
 })
--- part4/bloglist/utils/for_testing.js
+++ part4/bloglist/utils/for_testing.js
@@ -1,3 +1,5 @@
+const groupBy = require('lodash.groupby')
+
 const dummy = () => {
   return 1
 }
@@ -15,12 +17,63 @@ const favouriteBlog = (blogs) => {
   const likes = blogs.map(blog => blog.likes)
   let maxLike = Math.max(...likes)
   let blogData = blogs.filter(blog => blog.likes === maxLike)[0]
-  return {author: blogData.author, likes: blogData.likes}
+  return {
+    title: blogData.title,
+    author: blogData.author,
+    likes: blogData.likes
+  }
+}
+
+
+const mostBlogs = (blogs) => {
+  const groupedBlogs = groupBy(blogs, ({author}) => author)
+  let newBlogs = {}
+
+  for (blog in groupedBlogs) {
+    let noOfBlogs = groupedBlogs[blog].length
+    newBlogs[blog] = noOfBlogs
+  }
+
+  let total = Object.values(newBlogs)
+  let maxNoOfBlogs = Math.floor(Math.max(...total))
+  let authorWithMostBlogs = Object.keys(newBlogs).find(key => newBlogs[key] === maxNoOfBlogs)
+
+  return {
+    author: authorWithMostBlogs,
+    blogs: maxNoOfBlogs
+  }
+
+}
+
+
+const mostLikes = (blogs) => {
+  // collate authors and their blog posts.
+  const groupedBlogs = groupBy(blogs, ({author}) => author)
+  let newBlogs = {}
+  
+  for (author in groupedBlogs) {
+    // aggregate all likes gotten from an author's posts
+    let totalAuthorLikes = groupedBlogs[author].reduce((sum, blog) => {
+      return sum + blog.likes
+    }, 0)
+    newBlogs[author] = totalAuthorLikes
+  }
+
+  let arrayOfLikes = Object.values(newBlogs)
+  let maxNoOfLikes = Math.floor(Math.max(...arrayOfLikes))
+  let authorWithMostLikes = Object.keys(newBlogs).find(key => newBlogs[key] === maxNoOfLikes)
+
+  return {
+    author: authorWithMostLikes,
+    likes: maxNoOfLikes
+  }
 }
 
 
 module.exports = {
   dummy,
   totalLikes,
-  favouriteBlog
+  favouriteBlog,
+  mostBlogs,
+  mostLikes,
 }
